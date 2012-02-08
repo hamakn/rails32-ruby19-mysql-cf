6d6b7fa37e6fb6c284390566fea28e55437ef5d5
Katsunori Kawaguchi <hamakata@gmail.com>
2012-02-08 22:01:17 +0900

    rails new rails32-ruby19-mysql -T -d mysql --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails32Ruby19Mysql::Application.config.secret_token = 'c7ec274357af4a2cbd3805e5737a3f2a0628ea1664b4915bfdb816412ce4179b0f2908280421285d6767cd2c2165f085e9b68e0d3ba0ef84b991bf32a984464b'
